37e742f9b9709f31c37721262cf771fa5117d79e
Merthynz <rerive@gmail.com>
2023-03-25 12:55:27 -0400

    Added DataHelper to keep the database in sync and added setup environment variables  link for email service

---
+++ Helpers/DataHelper.cs
@@ -0,0 +1,18 @@
+﻿using ContactPro.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactPro.Helpers
+{
+    public static class DataHelper
+    {
+        public static async Task ManageDataAsync
+            (IServiceProvider svcProvider)
+        {
+            // Get an instance of the db application context
+            var dbContextSvc = svcProvider.GetRequiredService<ApplicationDbContext>();
+
+            // Migration: This is equivalent to update-database
+            await dbContextSvc.Database.MigrateAsync();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -30,6 +30,11 @@ builder.Services.Configure<MailSettings>(builder.Configuration.GetSection("MailS
 
 var app = builder.Build();
 
+var scope = app.Services.CreateScope();
+
+// Get the database update with the latest migrations
+await DataHelper.ManageDataAsync(scope.ServiceProvider);
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
